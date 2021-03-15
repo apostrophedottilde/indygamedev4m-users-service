d59006d378e9ba73affc1fbbab09b426437b2a52
apostrophedottilde <apostrophedottilde@yahoo.com>
2021-03-15 12:21:34 +0000

    added cicd pipeline

--- resources/lambda/userTableUpdated.ts
+++ resources/lambda/userTableUpdated.ts
@@ -1,4 +1,4 @@
-import { DynamoDBStreamEvent } from 'aws-lambda';
+import { DynamoDBRecord, DynamoDBStreamEvent } from 'aws-lambda';
 import { SNS, config } from 'aws-sdk';
 
 config.update({ region: 'eu-west-1' });
@@ -6,12 +6,16 @@ const sns = new SNS({ apiVersion: '2010-03-31' })
 
 export async function handler(event: DynamoDBStreamEvent): Promise<null> {
     try {
-        const response = await sns.publish({
-            Message: 'MESSAGE_TEXT', /* required */
-            TopicArn: process.env.userDeregisteredTopicArn!
-        }).promise();
-        console.log(response);
-        console.log("did at least try inniut")
+        event.Records.forEach(async (e: DynamoDBRecord) => {
+            if(e.eventName === 'REMOVE') { 
+                const response = await sns.publish({
+                    Message: `User ${e.dynamodb!.Keys!.ID} has been deleted`,
+                    TopicArn: process.env.userDeregisteredTopicArn!
+                }).promise();
+                console.log(response);
+                console.log("did at least try inniut")
+            }
+        });
         return null;
     } catch (err) {
         console.log(err)
