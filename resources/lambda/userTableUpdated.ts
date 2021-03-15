a0430ffa831bb17a8a097bcb4bd8a98464147903
apostrophedottilde <apostrophedottilde@yahoo.com>
2021-03-15 15:33:59 +0000

    added check to ensure sns topic only published on removal

--- resources/lambda/userTableUpdated.ts
+++ resources/lambda/userTableUpdated.ts
@@ -4,13 +4,13 @@ import { SNS, config } from 'aws-sdk';
 config.update({ region: 'eu-west-1' });
 const sns = new SNS({ apiVersion: '2010-03-31' })
 
-export async function handler(event: DynamoDBStreamEvent): Promise<any> {
+export async function handler(event: DynamoDBStreamEvent): Promise<any | null> {
     try {
         const response = event.Records.map(async (e: any) => {
             console.log(e.eventName);
-            if (e.eventName == "REMOVE") {
+            if (e.eventName == 'REMOVE') {
                 console.log(event)
-                return sns.publish({
+                return await sns.publish({
                     Message: `User has been deleted`,
                     TopicArn: process.env.userDeregisteredTopicArn!
                 }).promise();
